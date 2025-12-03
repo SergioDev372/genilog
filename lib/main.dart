8ec20620d835413e64393ea4ba7cd39a1d03bead
xavier777501 <xavier7.com@gmail.com>
2025-12-03 15:26:59 +0100

    Mettre à jour le titre de l'app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Demoniaque',
+  title: 'demon mechant'
       theme: ThemeData(
         // This is the theme of your application.
         //
